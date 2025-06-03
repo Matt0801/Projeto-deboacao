eecc89431db000bad22315e469c89da8bef15602
Matth1 <106870686+Matt0801@users.noreply.github.com>
2025-06-03 13:23:53 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,27 @@
 import os
 from flask import Flask, request, jsonify
+from flask import abort
 from openai import OpenAI
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://deboaacao.vercel.app"])
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("key"))
+
+key = os.getenv("key") #colocado no render
 
 @app.route('/chat', methods=['POST'])
 def chat():
+    origin = request.headers.get('Origin')
+    if origin != "https://deboaacao.vercel.app":
+        abort(403)
+        
+    token = request.headers.get('Authorization')
+
+    if token != f"Bearer {key}":
+        return jsonify({'error': 'Unauthorized'}), 401
+    
     data = request.json
     user_input = data.get('message')
 
